a9ed13330c001b33494f4373e80a858b534442df
dominik-zeglen <flesz3@o2.pl>
2019-06-27 11:47:26 +0200

    Fix success messages

--- saleor/static/dashboard-next/types/globalTypes.ts
+++ saleor/static/dashboard-next/types/globalTypes.ts
@@ -11,6 +11,11 @@ export enum AddressTypeEnum {
   SHIPPING = "SHIPPING",
 }
 
+export enum AttributeInputTypeEnum {
+  DROPDOWN = "DROPDOWN",
+  MULTISELECT = "MULTISELECT",
+}
+
 export enum AttributeTypeEnum {
   PRODUCT = "PRODUCT",
   VARIANT = "VARIANT",
@@ -248,14 +253,21 @@ export interface AddressInput {
 }
 
 export interface AttributeAssignInput {
-  attributeId: string;
-  attributeType: AttributeTypeEnum;
+  id: string;
+  type: AttributeTypeEnum;
 }
 
 export interface AttributeCreateInput {
+  inputType?: AttributeInputTypeEnum | null;
   name: string;
   slug?: string | null;
   values?: (AttributeValueCreateInput | null)[] | null;
+  valueRequired?: boolean | null;
+  isVariantOnly?: boolean | null;
+  visibleInStorefront?: boolean | null;
+  filterableInStorefront?: boolean | null;
+  filterableInDashboard?: boolean | null;
+  storefrontSearchPosition?: number | null;
 }
 
 export interface AttributeInput {
@@ -268,6 +280,12 @@ export interface AttributeUpdateInput {
   slug?: string | null;
   removeValues?: (string | null)[] | null;
   addValues?: (AttributeValueCreateInput | null)[] | null;
+  valueRequired?: boolean | null;
+  isVariantOnly?: boolean | null;
+  visibleInStorefront?: boolean | null;
+  filterableInStorefront?: boolean | null;
+  filterableInDashboard?: boolean | null;
+  storefrontSearchPosition?: number | null;
 }
 
 export interface AttributeValueCreateInput {
@@ -276,8 +294,10 @@ export interface AttributeValueCreateInput {
 }
 
 export interface AttributeValueInput {
-  slug: string;
-  value: string;
+  id?: string | null;
+  name?: string | null;
+  slug?: string | null;
+  values: (string | null)[];
 }
 
 export interface AuthorizationKeyInput {
@@ -591,6 +611,7 @@ export interface VoucherInput {
   categories?: (string | null)[] | null;
   minAmountSpent?: any | null;
   countries?: (string | null)[] | null;
+  applyOncePerOrder?: boolean | null;
 }
 
 //==============================================================
